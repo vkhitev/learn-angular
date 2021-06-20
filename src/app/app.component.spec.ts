37f9eecde785cf784a4d096b75fdd1039e2fb7e3
Vlad Khitev <vkhitev@gmail.com>
2021-06-21 02:07:31 +0300

    feat: bootstrap products app template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,10 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'learn-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('learn-angular');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { Logger } from 'src/services/Logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learn-angular';
-
-  constructor (private logger: Logger) {}
-
-  handleLog() {
-    this.logger.log('Hello')
-  }
-}
+export class AppComponent {}
