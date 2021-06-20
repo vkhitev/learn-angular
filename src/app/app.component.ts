1ff5c65c9aa355b6bc0078c663d21cf06c451bd1
Vlad Khitev <vkhitev@gmail.com>
2021-06-21 01:27:53 +0300

    feat: add logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Logger } from 'src/services/Logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'learn-angular';
+
+  constructor (private logger: Logger) {}
+
+  handleLog() {
+    this.logger.log('Hello')
+  }
 }
---
+++ src/services/Logger.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class Logger {
+  log(value: any) {
+    console.log(value);
+  }
+}
