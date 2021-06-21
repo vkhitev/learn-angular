ecf81f19fecb15138e09ac9b402e15acb07e1375
Vlad Khitev <vkhitev@gmail.com>
2021-06-21 12:32:10 +0300

    feat: add shopping cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AppComponent } from './app.component'
 import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component'
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component'
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent, CartComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
