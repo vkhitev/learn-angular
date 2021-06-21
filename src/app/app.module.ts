ab280e169b674d00d2d9254f7f856cc2a1960ac2
Vlad Khitev <vkhitev@gmail.com>
2021-06-21 12:50:23 +0300

    feat: add shipping prices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component'
 import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductListComponent } from './product-list/product-list.component'
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component'
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component'
+import { ProductDetailsComponent } from './product-details/product-details.component'
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent, CartComponent],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
