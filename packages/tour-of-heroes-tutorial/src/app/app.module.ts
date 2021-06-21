d59b2f220ece2f4813cb8f2b09158c2e865dfc93
Vlad Khitev <vkhitev@gmail.com>
2021-06-21 13:48:25 +0300

    feat: add heroes component

--- packages/tour-of-heroes-tutorial/src/app/app.module.ts
+++ packages/tour-of-heroes-tutorial/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HeroesComponent } from './heroes/heroes.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
